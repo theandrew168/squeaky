1eec35bb6af7dbed02f5a6065c59372d55eaca63
theandrew168 <steveday168@gmail.com>
2020-07-05 23:05:58 -0500

    Add MPC library to the project

--- Makefile
+++ Makefile
@@ -15,16 +15,24 @@ LDLIBS  =
 # compile linenoise as C99 (plus POSIX.1-2008) with debug symbols
 LINENOISE_CFLAGS = -std=c99 -D_POSIX_C_SOURCE=200809L -fPIC -g -Og
 
+# https://github.com/orangeduck/mpc
+# compile mpc as C99 with debug symbols
+MPC_CFLAGS = -std=c99 -fPIC -g -Og
+
 default: lispy
 all: liblispy.a liblispy.so lispy
 
 liblispy_sources =  \
-  src/linenoise.c
+  src/linenoise.c   \
+  src/mpc.c
 liblispy_objects = $(liblispy_sources:.c=.o)
 
 src/linenoise.o: src/linenoise.c src/linenoise.h
 	@echo "CC      $@"
 	@$(CC) -c $(LINENOISE_CFLAGS) -o $@ $<
+src/mpc.o: src/mpc.c src/mpc.h
+	@echo "CC      $@"
+	@$(CC) -c $(MPC_CFLAGS) -o $@ $<
 
 liblispy.a: $(liblispy_objects)
 	@echo "STATIC  $@"
--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 #include "linenoise.h"
+#include "mpc.h"
 
 static const char LISPY_PROMPT[] = "lispy> ";
 static const char LISPY_HISTORY_FILE[] = ".lispy_history";
@@ -9,6 +10,18 @@ static const char LISPY_HISTORY_FILE[] = ".lispy_history";
 int
 main(int argc, char* argv[])
 {
+    mpc_parser_t* Number   = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expr     = mpc_new("expr");
+    mpc_parser_t* Lispy    = mpc_new("lispy");
+    
+    mpca_lang(MPCA_LANG_DEFAULT,
+        "number   : /-?[0-9]+/ ;"
+        "operator : '+' | '-' | '*' | '/' ;"
+        "expr     : <number> | '(' <operator> <expr>+ ')' ;"
+        "lispy    : /^/ <operator> <expr>+ /$/ ;",
+        Number, Operator, Expr, Lispy);
+
     puts("Lispy Version 0.0.1");
     puts("Press Ctrl+c to exit\n");
 
@@ -24,10 +37,19 @@ main(int argc, char* argv[])
         linenoiseHistoryAdd(line);
         linenoiseHistorySave(LISPY_HISTORY_FILE);
 
-        printf("You wrote: %s\n", line);
+        mpc_result_t r = { 0 };
+        if (mpc_parse("<stdin>", line, Lispy, &r) != 0) {
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        } else {
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
 
         linenoiseFree(line);
     }
+
+    mpc_cleanup(4, Number, Operator, Expr, Lispy);
 
     return EXIT_SUCCESS;
 }

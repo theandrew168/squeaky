837c185944fce26aa90ddf4a74f60fa4ac281246
theandrew168 <steveday168@gmail.com>
2020-07-16 14:04:13 -0500

    Add character literals

--- src/object.h
+++ src/object.h
@@ -5,24 +5,29 @@
 
 enum object_type {
     OBJECT_TYPE_UNDEFINED = 0,
-    OBJECT_TYPE_BOOLEAN,
     OBJECT_TYPE_FIXNUM,
+    OBJECT_TYPE_BOOLEAN,
+    OBJECT_TYPE_CHARACTER,
 };
 
 struct object {
     int type;
     union {
         struct {
+            long value;
+        } fixnum;
+        struct {
             bool value;
         } boolean;
         struct {
-            long value;
-        } fixnum;
+            char value;
+        } character;
     } data;
 };
 
-void object_make_boolean(struct object* object, bool value);
 void object_make_fixnum(struct object* object, long value);
+void object_make_boolean(struct object* object, bool value);
+void object_make_character(struct object* object, char value);
 void object_print(const struct object* object);
 
 #endif
--- src/reader.c
+++ src/reader.c
@@ -40,6 +40,76 @@ reader_skip_whitespace(FILE* stream)
     }
 }
 
+static int
+reader_skip_expected_string(FILE* stream, const char* str)
+{
+    while (*str != '\0') {
+        int c = getc(stream);
+        if (c != *str) {
+            fprintf(stderr, "unexpected character: '%c'\n", c);
+            return READER_ERROR;
+        }
+        str++;
+    }
+
+    return READER_OK;
+}
+
+static int
+reader_peek_expected_delimiter(FILE* stream)
+{
+    if (!reader_is_delimiter(reader_peek(stream))) {
+        fprintf(stderr, "character literal not followed by delimiter\n");
+        return READER_ERROR;
+    }
+
+    return READER_OK;
+}
+
+//static int reader_read_boolean(FILE* stream, struct object* object);
+//static int reader_read_fixnum(FILE* stream, struct object* object);
+
+static int
+reader_read_character(FILE* stream, struct object* object)
+{
+    int c = getc(stream);
+    switch (c) {
+        case EOF:
+            fprintf(stderr, "incomplete character literal\n");
+            return READER_ERROR;
+        case 's':
+            if (reader_peek(stream) == 'p') {
+                int rc = reader_skip_expected_string(stream, "pace");
+                if (rc != READER_OK) return rc;
+
+                rc = reader_peek_expected_delimiter(stream);
+                if (rc != READER_OK) return rc;
+
+                object_make_character(object, ' ');
+                return READER_OK;
+            }
+            break;
+        case 'n':
+            if (reader_peek(stream) == 'e') {
+                int rc = reader_skip_expected_string(stream, "ewline");
+                if (rc != READER_OK) return rc;
+
+                rc = reader_peek_expected_delimiter(stream);
+                if (rc != READER_OK) return rc;
+
+                object_make_character(object, '\n');
+                return READER_OK;
+            }
+            break;
+    }
+
+    reader_peek_expected_delimiter(stream);
+    object_make_character(object, c);
+    return READER_OK;
+}
+
+//static int reader_read_string(FILE* stream, struct object* object);
+
 int
 reader_read(FILE* stream, struct object* object)
 {
@@ -58,8 +128,10 @@ reader_read(FILE* stream, struct object* object)
             case 'f':
                 object_make_boolean(object, false);
                 break;
+            case '\\':
+                return reader_read_character(stream, object);
             default:
-                fprintf(stderr, "unknown boolean literal\n");
+                fprintf(stderr, "unknown boolean or character literal\n");
                 return READER_ERROR;
         }
     } else if (isdigit(c) || (c == '-' && isdigit(reader_peek(stream)))) {  // check for fixnum

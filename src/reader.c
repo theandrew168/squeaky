4611e8dc6af603553c17fb26732bf0a66743a7a6
theandrew168 <steveday168@gmail.com>
2020-08-11 18:12:59 -0500

    More design work on the reader

--- src/reader.c
+++ src/reader.c
@@ -8,66 +8,49 @@
 
 // R5RS 7.1.1: Lexical Structure
 
-static bool
-is_whitespace(int c)
-{
-    return strchr(" \f\n\r\t\v", c) != NULL;
-}
+#define is_whitespace(c)  \
+  (strchr(" \f\n\r\t\v", c) != NULL)
+#define is_letter(c)  \
+  ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+#define is_digit(c)  \
+  (c >= '0' && c <= '9')
+#define is_delimiter(c)  \
+  (is_whitespace(c) || strchr("()\";", c) != NULL)
+#define is_special_initial(c)  \
+  (strchr("!$%&*/:<=>?^_~", c) != NULL)
+#define is_initial(c)  \
+  (is_letter(c) || is_special_initial(c))
+#define is_special_subsequent(c)  \
+  (strchr("+-.@", c) != NULL)
+#define is_subsequent(c)  \
+  (is_initial(c) || is_digit(c) || is_special_subsequent(c))
+#define is_peculiar_identifier(s)  \
+  (*s == '+' || *s == '-' || strncmp("...", s, 3) == 0)
 
-static bool
-is_letter(int c)
+static int
+advance(FILE* fp)
 {
-    return (c >= 'A' && c <= 'Z') ||
-           (c >= 'a' && c <= 'z');
-}
-
-static bool
-is_digit(int c)
-{
-    return c >= '0' && c <= '9';
-}
-
-static bool
-is_special_initial(int c)
-{
-    return strchr("!$%&*/:<=>?^_~", c) != NULL;
-}
-
-static bool
-is_initial(int c)
-{
-    return is_letter(c) ||
-           is_special_initial(c);
-}
-
-static bool
-is_special_subsequent(int c)
-{
-    return strchr("+-.@", c) != NULL;
-}
-
-static bool
-is_subsequent(int c)
-{
-    return is_initial(c) ||
-           is_digit(c) ||
-           is_special_subsequent(c);
-}
-
-static bool
-is_peculiar_identifier(const char* s)
-{
-    return *s == '+' ||
-           *s == '-' ||
-           strncmp("...", s, 3) == 0;
+    // grab the next character
+    return fgetc(fp);
 }
 
 static int
 peek(FILE* fp)
 {
+    // grab the next character without changing position
     int c = fgetc(fp);
     ungetc(c, fp);
     return c;
+}
+
+static void
+expect_delimiter(FILE* fp)
+{
+    // expect a delimiter to follow some token otherwise raise an error
+    if (!is_delimiter(peek(fp))) {
+        fprintf(stderr, "reader: expected delimiter\n");
+        exit(EXIT_FAILURE);
+    }
 }
 
 static void
@@ -93,9 +76,9 @@ eat_whitespace(FILE* fp)
 static void
 eat_string(FILE* fp, const char* s)
 {
-    int c;
+    // ensure that an expected string comes next in the file
     while (*s != '\0') {
-        c = fgetc(fp);
+        int c = fgetc(fp);
         if (c != *s) {
             fprintf(stderr, "reader: unexpected character: want '%c', got '%c'\n", *s, c);
             exit(EXIT_FAILURE);
@@ -105,13 +88,53 @@ eat_string(FILE* fp, const char* s)
 }
 
 struct value*
+read_string(FILE* fp)
+{
+    // skip the leading quote
+    advance(fp);
+    long start = ftell(fp);
+    if (start == -1) {
+        perror("failed to capture file position");
+        exit(EXIT_FAILURE);
+    }
+
+    int c;
+    while ((c = fgetc(fp)) != EOF) {
+        if (c == '"') break;
+    }
+
+    if (feof(fp)) {
+        fprintf(stderr, "reader: unterminated string\n");
+        exit(EXIT_FAILURE);
+    }
+
+    long end = ftell(fp);
+    long size = end - start + 1;
+
+    // make a buffer, roll back, fread, copy to value, free?
+    // there has to be a cleaner way
+
+    // skip the tailing quote
+    advance(fp);
+//    return value;
+}
+
+struct value*
 reader_read(FILE* fp)
 {
     assert(fp != NULL);
+
+    eat_whitespace(fp);
 
     if (feof(fp)) {
         return value_make_eof();
     }
 
-    return EMPTY_LIST;
+    int c = peek(fp);
+    if (c == '"') {
+        return read_string(fp);
+    }
+
+    fprintf(stderr, "reader: invalid syntax\n");
+    exit(EXIT_FAILURE);
 }
